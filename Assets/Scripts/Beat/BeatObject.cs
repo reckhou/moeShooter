de27583fbb399cc3ebac5d3830ba8d3c074ae27e
Reck Hou <reckhou@gmail.com>
2014-09-22 10:23:48 +0800

    Add beat scene demo.

---
+++ Assets/Scripts/Beat/BeatObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatObject : MonoBehaviour {
+	protected int curBeat { get; set; }
+	public int beatStatCnt { get; set; }
+	public Vector3 originScale { get; set; }
+
+	void Start() {
+		Init();
+	}
+
+	public virtual void Init() {
+		beatStatCnt = GlobalConfig.BeatStatCnt;
+		originScale = transform.localScale;
+	}
+
+	public virtual void Beat() {
+		curBeat++;
+		if (curBeat >= beatStatCnt) {
+			curBeat = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Beat/BeatObjectDefault.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatObjectDefault : BeatObject {
+	public float BarTime { get; set; }
+	public float AnimationPerBar = 2;
+
+
+	public override void Init() {
+		base.Init();
+		updateSpeed();
+  }
+
+	public override void Beat() {
+		base.Beat();
+		if (curBeat == 0) {
+			updateSpeed();
+		}
+  }
+
+	private void updateSpeed() {
+		/* All animation is 1 sec length by default */
+		float animationTime = BarTime / AnimationPerBar;
+		Animator anim = GetComponent<Animator>();
+		anim.speed = 1.0f / animationTime;
+//		print (1.0f / animationTime);
+  }
+}
